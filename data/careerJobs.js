8395a5b1e559705104d53a4204ec8e1123f542f3
sabyr-cpu <sab.sabyr@gmail.com>
2021-10-31 14:34:27 +0600

    fix: get careerJobs

--- data/careerJobs.js
+++ data/careerJobs.js
@@ -1,13 +1,17 @@
+const careerJobsModel = require('../careerJobs.model.js')
+const contactMeModel = require('../contact.model.js')
 const careerJobs = [
-    {
-        career_job_title: 'Front End Developer'
-    },
-    {
-        career_job_title: 'Freelancer'
-    },
-    {
-        career_job_title: 'Web Designer'
-    }
+  {
+    career_job_title: 'Front End Developer'
+  },
+  {
+    career_job_title: 'Freelancer'
+  },
+  {
+    career_job_title: 'Web Designer'
+  }
 ]
 
-module.exports = careerJobs
+module.exports = async() => {
+    return await careerJobsModel.find({})
+}
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const chalk = require('chalk');
-const contact = require('./contact.model.js');
-const app = express();
-const PORT = 9000;
-const DBURL = 'mongodb+srv://admin:admin@cluster0.qesqn.mongodb.net/form?retryWrites=true&w=majority';
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const chalk = require('chalk')
+const contact = require('./contact.model.js')
+const app = express()
+const PORT = 9000
+const DBURL =
+  'mongodb+srv://admin:admin@cluster0.qesqn.mongodb.net/form?retryWrites=true&w=majority'
 const careerJobs = require('./data/careerJobs')
 const portfolioProjects = require('./data/portfolioProjects')
 const skillsMarkup = require('./data/skillsMarkup')
@@ -19,14 +20,19 @@ const availability = require('./data/availability')
 
 app.listen(PORT, () => {
   console.log(chalk.green(`Listening on port ${PORT}`))
-  mongoose.connect(DBURL, {
+  mongoose
+    .connect(DBURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-  }).then(_ => {
-      console.log(chalk.green('Succesfully connected to database'))
-  }, err => {
-      console.log(chalk.red(err))
-  })
+    })
+    .then(
+      _ => {
+        console.log(chalk.green('Succesfully connected to database'))
+      },
+      err => {
+        console.log(chalk.red(err))
+      }
+    )
 })
 
 app.use(express.json())
@@ -38,21 +44,35 @@ app.post('/api/form', (req, res) => {
     subject: req.body.subject,
     email: req.body.email,
     message: req.body.message
-  }).save((err) => {
-    if(!err) res.json({success: true})
-    else res.json({success: false, error: err})
+  }).save(err => {
+    if (!err) res.json({ success: true })
+    else res.json({ success: false, error: err })
   })
 })
 
 // app.get('/api/form', (req, res) => res.status(200).send(сюда надо ее засунуть
 
-app.get('/api/career-jobs', (req, res) => res.send(careerJobs))
-app.get('/api/portfolio-projects', (req, res) => res.status(200).send(portfolioProjects))
+app.get('/api/career-jobs', async(req, res) => {
+  res.send(await careerJobs())
+
+  console.log(await careerJobs())
+})
+app.get('/api/portfolio-projects', (req, res) =>
+  res.status(200).send(portfolioProjects)
+)
 app.get('/api/skills-level', (req, res) => res.status(200).send(skillsMarkup))
-app.get('/api/portfolio-projects-tabs', (req, res) => res.status(200).send(portfolioProjectsTabs))
-app.get('/api/technologies-items', (req, res) => res.status(200).send(technologiesItems))
+app.get('/api/portfolio-projects-tabs', (req, res) =>
+  res.status(200).send(portfolioProjectsTabs)
+)
+app.get('/api/technologies-items', (req, res) =>
+  res.status(200).send(technologiesItems)
+)
 app.get('/api/technologies', (req, res) => res.status(200).send(technologies))
-app.get('/api/questions-answers', (req, res) => res.status(200).send(questionsanswers))
+app.get('/api/questions-answers', (req, res) =>
+  res.status(200).send(questionsanswers)
+)
 app.get('/api/timeline', (req, res) => res.status(200).send(timelineCards))
-app.get('/api/contact-details' ,(req, res) => res.status(200).send(contactDetails))
+app.get('/api/contact-details', (req, res) =>
+  res.status(200).send(contactDetails)
+)
 app.get('/api/availability', (req, res) => res.status(200).send(availability))
