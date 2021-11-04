9e0ccb828cac54225e9ebb48223b40914b6eb89f
ansagang <ansagaang@gmail.com>
2021-11-04 12:49:40 +0600

    f

--- models/portfolioProjects.model.js
+++ models/portfolioProjects.model.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose')
 const portfolioProjectsSchema = new mongoose.Schema({
     project_title: String,
     project_overview: String,
-    project_poster: [{ src: String, title: String }],
-    project_lang: [{ name: String }],
+    project_poster: Array,
+    project_lang: Array,
     project_logo: String,
     project_theme: String,
     project_theme_id: String,
     project_link: String,
     project_favourite:Boolean,
-    project_id: String
+    project_id: String,
+    project_year:String
 })
 
 module.exports = mongoose.model('portfolio_projects', portfolioProjectsSchema)
